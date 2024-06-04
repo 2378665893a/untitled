d4a991a86ca6706a77a5d73826d227005b6c2b3c
ZQJ <2378665893@qq.com>
2024-06-04 10:51:55 +0800

    main commit2

--- src/main/Git/test.java
+++ src/main/Git/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaa git!");
         System.out.println("bbb git!");
+        System.out.println("main test");
     }
 }
